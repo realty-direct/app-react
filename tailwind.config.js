7a3702d627385d22920786489de8cd8abb00aeab
Realty Direct <admin@realtydirect.com.au>
2024-10-12 13:52:47 +1000

    Remove the index.css styling and update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/routes/**/*.{html,js,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
