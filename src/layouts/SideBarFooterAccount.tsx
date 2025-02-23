4463b9577d41dc5501b076a57e554a8be5c11f57
Realty Direct <admin@realtydirect.com.au>
2025-02-23 21:42:52 +1000

    Test Commit

--- src/layouts/SideBarFooterAccount.tsx
+++ src/layouts/SideBarFooterAccount.tsx
@@ -23,6 +23,8 @@ export default function SidebarFooterAccountPopover() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null); // ✅ State for menu
   const open = Boolean(anchorEl);
 
+  const x;
+
   // ✅ Define user account details
   const account = {
     id: user?.id ?? "guest",
