2fa8a4621e252d8c4ccb011856c7b836bb46e93d
Realty Direct <admin@realtydirect.com.au>
2025-06-16 16:17:51 +1000

    Prepare for github pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rd-dashboard-react/',
   esbuild: {
     jsxInject: `import React from 'react'`
   },
